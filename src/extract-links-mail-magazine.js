ed741377b85b29f6287a22b28cfe8aefaa4c385d
azu <azuciao@gmail.com>
2016-10-23 16:21:25 +0900

    feat: add extractLinkAndTitle

--- src/extract-links-mail-magazine.js
+++ src/extract-links-mail-magazine.js
@@ -2,6 +2,7 @@
 "use strict";
 import unique from "array-unique";
 import stripUTM from "strip-utm";
+const MapLike = require("map-like");
 const defaultBlackList = [
     // Mail
     /list-manage\.com/,
@@ -10,10 +11,14 @@ const defaultBlackList = [
     /campaign-archive.com/,
     /campaign-archive\d+.com/,
     // share
-    /^http:\/\/twitter.com\/intent\//,
-    /^http:\/\/www.facebook.com\/sharer\//
+    /^https?:\/\/twitter.com\/intent\//,
+    /^https?:\/\/www.facebook.com\/sharer\//,
+    /^https?:\/\/facebook.com\/sharer\//
 ];
 
+const defaultBlockTitleList = [
+    /^こちら$/
+];
 const hasNotTextSibling = (prev, next)=> {
     if (prev && next) {
         return prev.nodeName !== "#text" && next.nodeName !== "#text";
@@ -61,17 +66,37 @@ const isAbsoluteLink = (aLink) => {
  * @returns {String[]}
  */
 export function extractLinks(currentURL, document, options = {}) {
+    return extractLinkAndTitle(currentURL, document, options).map(([url]) => {
+        return url;
+    });
+}
+/**
+ * extract link in the document.
+ * @param {string} currentURL
+ * @param {Document} document Browser's document or Jsdom's document
+ * @param {Object} options
+ * @returns {String[]}
+ */
+export function extractLinkAndTitle(currentURL, document, options = {}) {
     const blackList = options.blackList || defaultBlackList;
-    let allLinks = document.querySelectorAll("a");
+    const blackTitleList = options.blackTitleList || defaultBlockTitleList;
+    const allLinks = document.querySelectorAll("a");
     const isNotSameURLWithCurrent = (url) => {
         return currentURL !== url;
+    };
+    const isNotEmptyTitle = (title) => {
+        return title && title.length > 0;
     };
     const removeBlackList = (url) => {
         return !blackList.some(blackRegExp => {
             return blackRegExp.test(url);
         });
     };
-
+    const removeBlackTitleList = (title) => {
+        return !blackTitleList.some(blackRegExp => {
+            return blackRegExp.test(title);
+        });
+    };
     const filterByOnlyChild = (aLink) => {
         let prev = aLink.previousSibling;
         let prevElement = aLink.previousElementSibling;
@@ -103,14 +128,27 @@ export function extractLinks(currentURL, document, options = {}) {
         return false;
     };
     const linkArray = Array.prototype.slice.call(allLinks);
+    const linkTitleMap = new MapLike();
+    linkArray.forEach((aLink) => {
+        linkTitleMap.set(aLink.href, aLink.getAttribute("title") || aLink.textContent);
+    });
     return unique(linkArray
         .filter(filterByOnlyChild)
         .filter(isAbsoluteLink)
         .map(aLink => {
-            return aLink.href;
+            const stripURL = stripUTM(aLink.href);
+            const title = aLink.textContent || aLink.getAttribute("title") || '';
+            const trimmedTitle = title.replace(/\n(\s+)?/g, "");
+            linkTitleMap.set(stripURL, trimmedTitle);
+            return stripURL;
         })
         .filter(removeBlackList)
         .filter(isNotSameURLWithCurrent)
-        .map(stripUTM)
-    );
+    ).map(url => {
+        return [url, linkTitleMap.get(url)];
+    }).filter(([url, title]) => {
+        return isNotEmptyTitle(title);
+    }).filter(([url, title]) => {
+        return removeBlackTitleList(title);
+    });
 }
--- test/extract-links-mail-magazine.js
+++ test/extract-links-mail-magazine.js
@@ -1,7 +1,7 @@
 import assert from "assert"
 import jsdom from "jsdom";
 import fs from "fs";
-import {extractLinks} from "../src/extract-links-mail-magazine";
+import {extractLinks, extractLinkAndTitle} from "../src/extract-links-mail-magazine";
 describe("extract-item-links-test", function() {
     it("should return URL list", function() {
         let ml = ["javascript", "frontrend", "node"];
@@ -14,7 +14,8 @@ describe("extract-item-links-test", function() {
                         if (err) {
                             return reject(err);
                         }
-                        let links = extractLinks(window.location.href, window.document);
+                        let links = extractLinkAndTitle(window.location.href, window.document);
+                        console.log(links);
                         console.log(type + " " + links.length);
                         assert(links.length > 10);
                         resolve();
@@ -26,43 +27,142 @@ describe("extract-item-links-test", function() {
         return Promise.all(promises);
     });
     context("JavaScriptWeeklyIssue306.html", () => {
-        it("should extract link", () => {
+        it("should extract link", (done) => {
             const expected = [
-                'http://thenewstack.io/javascript-grows-gets-foundation',
-                'http://2ality.com/2016/10/asynchronous-iteration.html',
-                'https://github.com/tc39/proposal-async-iteration',
-                'https://medium.com/@nodejs/node-js-v6-transitions-to-lts-be7f18c17159',
-                'https://compose.com/articles/connecting-to-compose-for-mysql',
-                'https://nczonline.net/blog/2016/10/the-ecmascript-2016-change-you-probably-dont-know',
-                'https://toddmotto.com/angular-2-forms-reactive',
-                'https://sitepoint.com/yarn-vs-npm',
-                'https://sitepoint.com/jquery-document-ready-plain-javascript',
-                'https://js.foundation/announcements/2016/10/17/Linux-Foundation-Unites-JavaScript-Community-Open-Web-Development',
-                'https://medium.com/google-developer-experts/angular-2-new-features-in-angular-2-1-94132b1888f0',
-                'http://emberjs.com/blog/2016/10/17/ember-2-9-released.html',
-                'https://github.com/facebook/jsx/issues/65',
-                'https://github.com/mozilla/addons-linter/issues/1000',
-                'https://developer.chrome.com/devsummit',
-                'http://thejsguy.com/2016/10/15/a-practical-introduction-to-es6-generator-functions.html',
-                'https://engineering.haus.com/dumb-es6-tricks-53ecadd1b29f',
-                'http://blog.wolksoftware.com/contributing-to-definitelytyped',
-                'https://daveceddia.com/visual-guide-to-state-in-react',
-                'https://blog.risingstack.com/async-await-node-js-7-nightly',
-                'http://alistapart.com/article/javascript-for-web-designers',
-                'https://hackernoon.com/using-yarn-with-docker-c116ad289d56',
-                'http://thenewcode.com/1152/Search-JavaScript-Arrays-Efficiently-with-includes-and-indexOf',
-                'https://sitepoint.com/javascript-3d-minecraft-editor',
-                'https://emberigniter.com/modern-bridge-ember-and-rails-5-with-json-api',
-                'http://theodo.fr/blog/2016/10/medium-like-image-loading-with-vue-js',
-                'https://youtube.com/playlist?list=PLNYkxOF6rcICc687SxHQRuo9TVNOJelSZ',
-                'https://medium.com/@alexnm/is-mvc-dead-for-the-frontend-35b4d1fe39ec',
-                'https://hackernoon.com/why-you-shouldnt-use-var-anymore-f109a58b9b70',
-                'https://sentry.io/for/javascript',
-                'https://github.com/getsentry/sentry',
-                'https://github.com/moinism/faltu',
-                'https://maxwellito.github.io/vivus',
-                'https://github.com/usdivad/mesing',
-                'http://usdivad.com/mesing'
+                [
+                    'http://thenewstack.io/javascript-grows-gets-foundation',
+                    'JavaScript Gets Its Own Foundation'
+                ],
+                [
+                    'http://2ality.com/2016/10/asynchronous-iteration.html',
+                    'An ES Proposal: Asynchronous Iteration'
+                ],
+                [
+                    'https://github.com/tc39/proposal-async-iteration',
+                    'asynchronous iteration'
+                ],
+                [
+                    'https://medium.com/@nodejs/node-js-v6-transitions-to-lts-be7f18c17159',
+                    'Node.js v6 Transitions to LTS'
+                ],
+                [
+                    'https://compose.com/articles/connecting-to-compose-for-mysql',
+                    'MySQL on Compose - hosted on AWS, GCP or SoftLayer'
+                ],
+                [
+                    'https://nczonline.net/blog/2016/10/the-ecmascript-2016-change-you-probably-dont-know',
+                    'The ECMAScript 2016 Change You Probably Don\'t Know'
+                ],
+                [
+                    'https://toddmotto.com/angular-2-forms-reactive',
+                    'Angular 2 Form Fundamentals: Reactive Forms'
+                ],
+                [
+                    'https://sitepoint.com/yarn-vs-npm',
+                    'Yarn vs npm: Everything You Need to Know'
+                ],
+                [
+                    'https://sitepoint.com/jquery-document-ready-plain-javascript',
+                    'Replacing jQuery\'s ready() with Plain JavaScript'
+                ],
+                [
+                    'https://js.foundation/announcements/2016/10/17/Linux-Foundation-Unites-JavaScript-Community-Open-Web-Development',
+                    'The Linux Foundation Unites JavaScript Community for Open Web Development'
+                ],
+                [
+                    'https://medium.com/google-developer-experts/angular-2-new-features-in-angular-2-1-94132b1888f0',
+                    'A Quick Review of New Features in Angular 2.1'
+                ],
+                [
+                    'http://emberjs.com/blog/2016/10/17/ember-2-9-released.html',
+                    'Ember.js 2.8-LTS, 2.9 and 2.10 Beta Released'
+                ],
+                [
+                    'https://github.com/facebook/jsx/issues/65',
+                    'Facebook Wants Your Input for JSX 2.0'
+                ],
+                [
+                    'https://github.com/mozilla/addons-linter/issues/1000',
+                    'Angular 1.x Banned From Use in Firefox Addons'
+                ],
+                [
+                    'https://developer.chrome.com/devsummit',
+                    'Chrome Dev Summit 2016'
+                ],
+                [
+                    'http://thejsguy.com/2016/10/15/a-practical-introduction-to-es6-generator-functions.html',
+                    'A Practical Introduction to ES6 Generator Functions'
+                ],
+                [
+                    'https://engineering.haus.com/dumb-es6-tricks-53ecadd1b29f',
+                    'Some Simple ES6 Syntax Tricks'
+                ],
+                [
+                    'http://blog.wolksoftware.com/contributing-to-definitelytyped',
+                    'How to Create Your Own TypeScript Type Definition Files (.d.ts)'
+                ],
+                [
+                    'https://daveceddia.com/visual-guide-to-state-in-react',
+                    'A Visual Guide to State in React'
+                ],
+                [
+                    'https://blog.risingstack.com/async-await-node-js-7-nightly',
+                    'Experimenting with async/await in Node.js 7 Nightly'
+                ],
+                [
+                    'http://alistapart.com/article/javascript-for-web-designers',
+                    'JavaScript for Web Designers: DOM Scripting'
+                ],
+                [
+                    'https://hackernoon.com/using-yarn-with-docker-c116ad289d56',
+                    'Using Yarn with Docker'
+                ],
+                [
+                    'http://thenewcode.com/1152/Search-JavaScript-Arrays-Efficiently-with-includes-and-indexOf',
+                    'Searching JavaScript Arrays Efficiently with includes and indexOf'
+                ],
+                [
+                    'https://sitepoint.com/javascript-3d-minecraft-editor',
+                    'Building a 3D Minecraft-Style Editor in JavaScript'
+                ],
+                [
+                    'https://emberigniter.com/modern-bridge-ember-and-rails-5-with-json-api',
+                    'Ember and Rails 5 with JSON API: A Modern Bridge'
+                ],
+                [
+                    'http://theodo.fr/blog/2016/10/medium-like-image-loading-with-vue-js',
+                    'Medium-Like Image Loading with Vue.js'
+                ],
+                [
+                    'https://youtube.com/playlist?list=PLNYkxOF6rcICc687SxHQRuo9TVNOJelSZ',
+                    'Polymer Summit 2016: Recordings of All Talks Are Now Available'
+                ],
+                [
+                    'https://medium.com/@alexnm/is-mvc-dead-for-the-frontend-35b4d1fe39ec',
+                    'Is MVC Dead for The Frontend?'
+                ],
+                [
+                    'https://hackernoon.com/why-you-shouldnt-use-var-anymore-f109a58b9b70',
+                    'Why You Shouldn’t Use ‘var’ Anymore'
+                ],
+                [
+                    'https://sentry.io/for/javascript',
+                    'Debugging JS errors in production'
+                ],
+                ['https://github.com/getsentry/sentry', '100% open source'],
+                [
+                    'https://github.com/moinism/faltu',
+                    'Faltu: Search Sort, Filter, and Limit An Array of Objects in MongoDB-esque Style'
+                ],
+                [
+                    'https://maxwellito.github.io/vivus',
+                    'Vivus.js: Bring SVGs to Life by \'Drawing\' Them via Animations'
+                ],
+                [
+                    'https://github.com/usdivad/mesing',
+                    'meSing.js: JavaScript Singing Synthesis Library'
+                ],
+                ['http://usdivad.com/mesing', 'demo here.']
             ];
             const content = fs.readFileSync(__dirname + "/fixtures/javascript/JavaScriptWeeklyIssue306.html", "utf-8");
             jsdom.env(
@@ -71,8 +171,9 @@ describe("extract-item-links-test", function() {
                     if (err) {
                         throw error;
                     }
-                    const links = extractLinks(window.location.href, window.document);
-                    assert.deepEqual(links, expected);
+                    const titleAndURLs = extractLinkAndTitle(window.location.href, window.document);
+                    assert.deepEqual(titleAndURLs, expected);
+                    done();
                 }
             );
 
